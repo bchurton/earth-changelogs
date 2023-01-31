f7cafb61c32390b6cdf2ac5691a4e81902bef843
bchurton <ben@churton.uk>
2023-01-31 19:08:30 +0000

    Initial commit

---
+++ src/pages/index1.md
@@ -0,0 +1,13 @@
+---
+layout: ../layouts/default.astro
+title: "Hello, World"
+version: "1.1.7"
+---
+
+```diff
++ Code knowledge increases by 75% when wearing thigh highs
++ Added more cute girls
++ Attempted to fix the Silly Cat duplication exploit
+- Rate of Silly Cat creation has increased by ten-fold
+- Removed MEE6, because who the fuck likes MEE6?
+```
---
+++ src/utils.js
@@ -0,0 +1,26 @@
+// code to get the latest .json file from the changelogs folder
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export function latestChangelog() {
+    console.log(__dirname);
+    const changelogFiles = fs.readdirSync(join(__dirname, '/changelogs'));
+    const latestChangelog = changelogFiles.sort().reverse()[0];
+    const changelog = JSON.parse(fs.readFileSync(join(__dirname, `/changelogs/${latestChangelog}`), 'utf8'));
+    return changelog;
+}
+
+export function getChangelog(version) {
+    if (!version.match(/^\d+\.\d+\.\d+$/)) return null;
+    const changelog = JSON.parse(fs.readFileSync(join(__dirname, `/changelogs/${version}.json`), 'utf8'));
+    return changelog;
+}
+
+export function listChangelogs() {
+    const changelogFiles = fs.readdirSync(join(__dirname, '/changelogs'));
+    return changelogFiles.map(file => file.replace('.json', ''));
+}
