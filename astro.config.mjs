9ee590b3574b75ad7ce82c07ba85d31237b6e30d
bchurton <ben@churton.uk>
2023-01-31 20:30:02 +0000

    netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-import cloudflare from "@astrojs/cloudflare";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
-  adapter: cloudflare()
+  adapter: netlify()
 });
